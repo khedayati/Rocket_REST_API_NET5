cb75800750e0e8cd88127c572a1d6418832d4cef
khedayati <91912775+khedayati@users.noreply.github.com>
2021-11-26 18:06:38 -0500

    updated columns controllers

--- Controllers/ColumnsController.cs
+++ Controllers/ColumnsController.cs
@@ -30,6 +30,7 @@ namespace RocketApi.Controllers
     [HttpGet("{id}/status")]
     public async Task<ActionResult<string>> GetColumnStatus(long id)
     {
+      // Find battery by its id
       var column = await _context.columns.FindAsync(id);
       if (column == null)
       {
@@ -43,11 +44,28 @@ namespace RocketApi.Controllers
     [HttpGet("update/{id}/{status}")]
     public async Task<dynamic> test(string status, long id)
     {
+      // Find battery by its id
       var column = await _context.columns.FindAsync(id);
 
+      // Check if the given status is either online, offline or intervention
+      if (!(status.Equals("Online") || status.Equals("online")) && 
+          !(status.Equals("Offline") || status.Equals("offline")) &&
+		      !(status.Equals("Intervention") || status.Equals("intervention"))) {
+            return Unauthorized();
+      }
+      // Change battery status
       column.status = status;
+      
+      // Save battery status
+      try
+      {
       await _context.SaveChangesAsync();
-
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        throw;
+      }
+      
       return column;
     }
   }
