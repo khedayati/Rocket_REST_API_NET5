3f1ad1a72fbda971896a808942be718445f91073
khedayati <91912775+khedayati@users.noreply.github.com>
2021-11-26 21:29:18 -0500

    added code from Phan

--- Controllers/buildingsController.cs
+++ Controllers/buildingsController.cs
@@ -49,12 +49,12 @@ namespace RocketApi.Controllers
         
         // GET: api/buildings/get-intervention-buildings
         [HttpGet("get-intervention-buildings")]
-        //public async Task<IEnumerable<buildings>> GetBuildings()
-        //{
-                    // GET: api/Buildings/InterventionList
-        //[HttpGet("InterventionList")]
         public async Task<ActionResult<List<buildings>>> GetToFixBuildings()
         {
+            // Find in batteries where its building_id is equal to building.id (battery that is part of the building)
+            // and in columns, find batteries.id  that is equal to column.battery_id (column that is part of the battery)
+            // and in elevtors, find column.id  that is equal to elevators.column_id (elevator that is part of the column)
+            // and add the building they're part of in the building list.
             IQueryable<buildings> buildings_list = from AllBuildings in _context.buildings
             join batteries in _context.batteries on AllBuildings.id equals batteries.building_id
             join columns in _context.columns on batteries.id equals columns.battery_id
@@ -65,84 +65,6 @@ namespace RocketApi.Controllers
             select AllBuildings;
 
             return await buildings_list.Distinct().ToListAsync();
-            /*
-        //}
-            List<buildings> buildings_list = new List<buildings>();
-
-            // Begin searching in buildings 
-            foreach (buildings building in await _context.buildings.ToListAsync())
-            {
-                var i = 0;
-                var j = 0;
-
-                // Loop through in batteries where its building_id is equal to building.id 
-                foreach (Battery battery in await _context.batteries.Where(b => b.building_id == building.id).ToListAsync())
-                {
-                    // Seek any battery that has an "intervention" status
-                    if (battery.status == "Intervention")
-                    {
-                        buildings_list.Add(building);
-                        i = 1;
-                        break;
-                    }
-                }
-                // Loop through in batteries where its building_id is equal to building.id 
-                foreach (Battery battery in await _context.batteries.Where(b => b.building_id == building.id).ToListAsync())
-                {
-                    // break from the loop since the column with an "intervention" status was found
-                    if (i == 1)
-                    {
-                        break;
-                    }
-                    var l = await _context.columns.Where(c => c.battery_id == battery.id).ToListAsync();
-
-                    // A battery contains a column, we must then loop through columns if in the loop above a battery was found
-                    foreach (Column column in await _context.columns.Where(c => c.battery_id == battery.id).ToListAsync())
-                    {
-                        // Seek any column that has an "intervention" status
-                        if (column.status == "Intervention")
-                        {   
-                            i = 1;
-                            buildings_list.Add(building);
-                            break;
-                        }
-                    }
-                }
-                // Loop through in batteries again but give the opportunity to search through elevators.
-                // Since a Battery may contain Columns and Elevators, if we found any one of the first two objects, we broke
-                // from their loops and we need to loop through them again to see if an elevator has the 'intervention' status
-                foreach (Battery battery in await _context.batteries.Where(b => b.building_id == building.id).ToListAsync())
-                {
-                    // break from the loop since the elevator with an "intervention" status was found
-                    if (j == 1)
-                    {
-                        break;
-                    }
-                    foreach (Column column in await _context.columns.Where(c => c.battery_id == battery.id).ToListAsync())
-                    {
-                        // break from the loop since the elevator with an "intervention" status was found
-                        if (j == 1)
-                        {
-                            break;
-                        }
-                        foreach (Elevator elevator in await _context.elevators.Where(e => e.column_id == column.id).ToListAsync())
-                        {
-                            // Seek any elevator that has an "intervention" status
-                            if (elevator.status == "Intervention")
-                            {
-                                buildings_list.Add(building);
-                                j = 1;
-                                i =1;
-                                break;
-                            }
-                        }
-                    }
-
-                }
-            }
-
-            return buildings_list;
-            */
         }
     }
 }
